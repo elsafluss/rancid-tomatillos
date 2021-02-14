7f59468337aa899696490f96cefb707173136086
mattdean <matthewallandean@gmail.com>
2021-02-14 09:43:08 -0700

    Add test for server and user error pages

--- cypress/integration/async_spec.js
+++ cypress/integration/async_spec.js
@@ -17,4 +17,43 @@ describe('As a user', () => {
         .should("have.attr", "src")
         .should("include", "https://media.giphy.com/media/ppyvw6iUQjdja/giphy.gif")
     })
+
+    it('displays an server error screen', () => {
+      cy.fixture("testMovieData.json")
+        .then((response) => {
+          
+          cy.intercept("https://rancid-tomatillos.herokuapp.com/api/v2/movies", {
+            statusCode: 500,
+            body: response
+          })
+        })
+        cy.visit('http://localhost:3000/')
+        cy
+        .get("section")
+        .get("h2").should("have.text", "This page is borked!")
+        .get("img")
+          .should("have.attr", "src")
+          .should("include", "https://media.giphy.com/media/10u6gt11vnm812/giphy.gif")
+        .get("h3")
+      })
+
+    it('displays an user error screen', () => {
+      cy.fixture("testMovieData.json")
+        .then((response) => {
+          
+          cy.intercept("https://rancid-tomatillos.herokuapp.com/api/v2/movies/", {
+            statusCode: 404,
+            body: response
+          })
+        })
+        cy.visit('http://localhost:3000/909090')
+        cy
+        .get("section")
+        .get("h2").should("have.text", "This page is borked!")
+        .get("img")
+          .should("have.attr", "src")
+          .should("include", "https://media.giphy.com/media/10u6gt11vnm812/giphy.gif")
+        .get("h3").first().click()
+        .url().should("eq", "http://localhost:3000/")
+      })
   })
