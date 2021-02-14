cb286cefb924cc913f9e233b622a3f7ace4e0412
Elsa Fluss <elsafluss@gmail.com>
2021-02-13 19:51:37 -0700

    Style loading screen

--- src/Loading/Loading.js
+++ src/Loading/Loading.js
@@ -5,7 +5,8 @@ import './Loading.scss'
 const Loading = () => {
     return (
         <section className="loading-container">
-            <h2>LOADING!</h2>
+            <h2>Please be patient, we're changing the reel!</h2>
+            <img className="borked-gif" src="https://media.giphy.com/media/ppyvw6iUQjdja/giphy.gif" alt="bork bork"></img>
         </section>
     )
 }
